99d8c600e06b619cffc7c679f1c69694d6503c35
kaleshavalicpt <42801338+kaleshavalicpt@users.noreply.github.com>
2018-09-19 19:25:45 +0530

    Update App.java

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -1,4 +1,4 @@
-package com.github;
+npackage com.github;
 
 /**
  * Hello again
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args )
     {
     	//Comment
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello jenkins!" );
     }
 }
 
